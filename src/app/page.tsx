ba4d69c4d0eb519081bc5d7c2a505ea94dde896b
Ayush syal <ayushsyal68@gmail.com>
2025-11-18 12:08:29 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+   <div className="flex flex-col items-center justify-center h-screen">
+    <Button>
+      Hello1
+    </Button>
+    <Button>
+      Hello2
+    </Button>
+    <Button>
+      Hello3
+    </Button>
+
+   </div>
+  );
+}
